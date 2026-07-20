65f24d913750b32484d6dc141d5c103d49b29445
bigfaveweb <bigfave1725@gmail.com>
2026-07-20 04:42:10 -1100

    Update tooplate-titan-script.js

--- tooplate-titan-script.js
+++ tooplate-titan-script.js
@@ -19,3 +19,26 @@ document.querySelectorAll(".mobile-menu a").forEach(link => {
     });
 
 });
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+    });
+
+    const result = await response.json();
+
+    if(result.success){
+        alert("Message sent successfully!");
+        form.reset();
+    }else{
+        alert("Something went wrong. Please try again.");
+    }
+
+});
