032b01523259b3480bff058a8be060468dec2be5
bigfaveweb <bigfave1725@gmail.com>
2026-07-20 04:23:29 -1100

    Update tooplate-titan-script.js

--- tooplate-titan-script.js
+++ tooplate-titan-script.js
@@ -1,1 +1,21 @@
 
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+
+    menuToggle.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        menuToggle.classList.remove("active");
+        mobileMenu.classList.remove("active");
+
+    });
+
+});
